9e70d69e1f22644d791d57928617e93aade51ba7
milosavljevicoa <milosavljevic246@gmail.com>
2026-06-21 23:10:13 +0200

    change identation to 4 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+local opt = vim.opt
+opt.tabstop = 4 -- Number of spaces a TAB character displays as
+opt.shiftwidth = 4 -- Number of spaces inserted for each indentation level
+opt.softtabstop = 4 -- Number of spaces inserted when the TAB key is pressed
+opt.expandtab = true -- Convert TABs to spaces
