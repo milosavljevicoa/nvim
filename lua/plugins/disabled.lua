ebe73ac1c0862dac48317d0fd298c95483b890ab
milosavljevicoa <milosavljevic246@gmail.com>
2026-06-21 21:44:22 +0200

    add all files

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "folke/flash.nvim",
+    keys = {
+      { "s", mode = { "n", "x", "o" }, false },
+      { "S", mode = { "n", "x", "o" }, false },
+    },
+  },
+}
